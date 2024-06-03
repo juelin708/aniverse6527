8063f84e2167b745b8397e4301be27fcc656f9e9
shuu4 <141632739+shuu4@users.noreply.github.com>
2024-06-03 11:06:47 +0800

    Update main.dart

--- aniverse/lib/main.dart
+++ aniverse/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Anicerse',
+      title: 'AniVerse',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
